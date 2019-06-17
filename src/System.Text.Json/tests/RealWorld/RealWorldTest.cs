7d5725bf053128c50ebe4908f4f15f226a9c64ee
Layomi Akinrinade <laakinri@microsoft.com>
2019-06-17 14:51:59 -0400

    Add test folder

---
+++ src/System.Text.Json/tests/RealWorld/RealWorldTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json.Serialization.Tests;
+using Xunit;
+
+namespace System.Text.Json.Tests.RealWorld
+{
+    public class RealWorldTest : ITestClass
+    {
+        public virtual void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Verify()
+        {
+            VerifySerializer();
+            VerifyDocument();
+            VerifyReader();
+            VerifyWriter();
+        }
+
+        public virtual string PayloadAsString => throw new NotImplementedException();
+
+        public byte[] PayloadAsUtf8Bytes => Encoding.UTF8.GetBytes(PayloadAsString);
+
+        public virtual void VerifySerializer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void VerifyDocument()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void VerifyReader()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void VerifyWriter()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void TestRealWorldUseCases()
+        {
+
+        }
+    }
+}
